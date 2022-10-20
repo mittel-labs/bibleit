b833a41153470eaf51af8cc1ea2815a36788a964
Felipe Mamud <trovatti@gmail.com>
2022-10-20 21:08:19 +0200

    version 0.0.5

--- src/bibleit/config.py
+++ src/bibleit/config.py
@@ -6,7 +6,7 @@ debug = False
 color = False
 label = False
 application = "bibleit"
-version = "0.0.4"
+version = "0.0.5"
 help = 'Type "help" for more information.'
 welcome = f"""
     Welcome to {application} v{version}

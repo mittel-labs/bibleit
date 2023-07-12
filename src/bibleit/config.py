3db06d461cb00c9c384598d7edeeb2e351baf9ff
Felipe Mamud <trovatti@gmail.com>
2023-07-12 23:44:52 +0200

    version 0.0.19

--- src/bibleit/config.py
+++ src/bibleit/config.py
@@ -6,7 +6,7 @@ __this = sys.modules[__name__]
 
 # General
 application = "bibleit"
-version = "0.0.18"
+version = "0.0.19"
 help = 'Type "help" for more information.'
 welcome = f"""
     Welcome to {application} v{version}
